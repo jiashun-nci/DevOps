7d4eba29bac8a52a37901481922b82c090c7dad1
jiashun-nci <x19134118@student.ncirl.ie>
2023-02-25 10:32:44 +0000

    Initial version of blog generated by generator-jhipster@7.9.3
    upload blog project

---
+++ src/main/webapp/app/entities/post/post.test-samples.ts
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs/esm';
+
+import { IPost, NewPost } from './post.model';
+
+export const sampleWithRequiredData: IPost = {
+  id: 35989,
+  title: 'Fantastic',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-02-24T09:17'),
+};
+
+export const sampleWithPartialData: IPost = {
+  id: 93202,
+  title: 'Seychelles',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-02-24T01:30'),
+};
+
+export const sampleWithFullData: IPost = {
+  id: 23240,
+  title: 'recontextualize virtual copying',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-02-24T03:11'),
+};
+
+export const sampleWithNewData: NewPost = {
+  title: 'withdrawal',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2023-02-24T04:42'),
+  id: null,
+};
+
+Object.freeze(sampleWithNewData);
+Object.freeze(sampleWithRequiredData);
+Object.freeze(sampleWithPartialData);
+Object.freeze(sampleWithFullData);
